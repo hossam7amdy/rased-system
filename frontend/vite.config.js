f244622836c5934c14c00e855146a04470f43775
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-06-14 19:10:13 +0300

    refactor: reformat wrangler configuration and reorder vite plugin imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
-
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
